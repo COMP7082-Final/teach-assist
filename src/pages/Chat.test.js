1877b08bcbfd4615b586df96e1d615ed385206c2
Lydia-l-YU <71059872+Lydia-l-YU@users.noreply.github.com>
2020-12-03 11:33:37 -0800

    Revert "Try merge dev to auth "

--- src/pages/Chat.test.js
+++
@@ -1,9 +0,0 @@
-import Chat from "./Chat"
-import {render, screen} from "@testing-library/react";
-
-test('renders Send Button for Chat Functionality', () => {
-    render(<Chat/>);
-    const buttonElement = screen.getByText("Send");
-    expect(buttonElement).toBeInTheDocument();
-});
-
